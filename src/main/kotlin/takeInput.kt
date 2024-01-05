61267452e9ca2a0305a4bc18a2e362d69f48e81a
Tejaskt <tejasktgeetanjali88@gmail.com>
2024-01-05 12:16:06 +0530

    how to take input in kotlin...

---
+++ src/main/kotlin/takeInput.kt
@@ -0,0 +1,13 @@
+fun main() {
+    println("here is the example to take input in kotlin using readLine() method")
+
+    print("Enter a number : ")
+    val n = readlnOrNull()
+
+    println()
+
+ print("Enter a string")
+    val id = readln()
+
+    println("first = $n\n second = $id")
+}
